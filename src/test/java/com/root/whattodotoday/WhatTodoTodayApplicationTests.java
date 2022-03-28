eefc12fb9de155fc932f6908fab28e57d20a088e
eunhye <eunhye_3@naver.com>
2022-03-28 10:54:56 +0900

    init

---
+++ src/test/java/com/root/whattodotoday/WhatTodoTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.whattodotoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTodoTodayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
